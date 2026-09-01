11e05a5f630bd384df6da4b73aecea2dee039666
Matias Gaglio <matiasignaciogaglio@gmail.com>
2026-09-01 10:01:32 -0300

    Agregar dataset 7.6

---
+++ data/dataset-v7.6.js
@@ -0,0 +1,33 @@
+const dataset = {
+  version: "7.6",
+  constructorHistory: [
+    {team:"Ferrari",values:[{year:1958,titles:0},{year:1961,titles:1},{year:1964,titles:2},{year:1975,titles:3},{year:1976,titles:4},{year:1977,titles:5},{year:1979,titles:6},{year:1982,titles:7},{year:1983,titles:8},{year:1999,titles:9},{year:2000,titles:10},{year:2001,titles:11},{year:2002,titles:12},{year:2003,titles:13},{year:2004,titles:14},{year:2007,titles:15},{year:2008,titles:16},{year:2024,titles:16}]},
+    {team:"Williams",values:[{year:1958,titles:0},{year:1980,titles:1},{year:1981,titles:2},{year:1986,titles:3},{year:1987,titles:4},{year:1992,titles:5},{year:1993,titles:6},{year:1994,titles:7},{year:1996,titles:8},{year:1997,titles:9},{year:2024,titles:9}]},
+    {team:"McLaren",values:[{year:1958,titles:0},{year:1974,titles:1},{year:1984,titles:2},{year:1985,titles:3},{year:1988,titles:4},{year:1989,titles:5},{year:1990,titles:6},{year:1991,titles:7},{year:1998,titles:8},{year:2024,titles:9}]},
+    {team:"Mercedes",values:[{year:1958,titles:0},{year:2014,titles:1},{year:2015,titles:2},{year:2016,titles:3},{year:2017,titles:4},{year:2018,titles:5},{year:2019,titles:6},{year:2020,titles:7},{year:2021,titles:8},{year:2024,titles:8}]},
+    {team:"Red Bull",values:[{year:1958,titles:0},{year:2010,titles:1},{year:2011,titles:2},{year:2012,titles:3},{year:2013,titles:4},{year:2022,titles:5},{year:2023,titles:6},{year:2024,titles:6}]}
+  ],
+  driverStandings: [
+    {position:1,name:"Lewis Hamilton",detail:"Pico: GP de España 2021",points:2002.1},
+    {position:2,name:"Max Verstappen",detail:"Pico: GP de Abu Dabi 2025",points:1976.3},
+    {position:3,name:"Valtteri Bottas",detail:"Pico: GP de Emilia-Romaña 2020",points:1893.1},
+    {position:4,name:"Nico Rosberg",detail:"Pico: GP de Abu Dabi 2016",points:1892.4},
+    {position:5,name:"Sebastian Vettel",detail:"Pico: GP de Canadá 2019",points:1868.4}
+  ],
+  constructorStandings: [
+    {position:1,name:"McLaren MCL39",detail:"Temporada 2025",points:1464.1},
+    {position:2,name:"Mercedes F1 W06",detail:"Temporada 2015",points:1382.9},
+    {position:3,name:"Red Bull RB19",detail:"Temporada 2023",points:1342.8},
+    {position:4,name:"Mercedes F1 W05",detail:"Temporada 2014",points:1322.7},
+    {position:5,name:"Mercedes F1 W10",detail:"Temporada 2019",points:1317.2}
+  ],
+  fanIndex: [
+    {position:1,name:"Ayrton Senna",score:28.04,visual:100},
+    {position:2,name:"Michael Schumacher",score:20.55,visual:73.3},
+    {position:3,name:"Juan M. Fangio",score:15.39,visual:54.9},
+    {position:4,name:"Jim Clark",score:9.15,visual:32.6},
+    {position:5,name:"Lewis Hamilton",score:8.88,visual:31.7}
+  ]
+};
+
+export default dataset;
